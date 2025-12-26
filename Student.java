f51cdb33159dfdd1f249757157066b455c054e4d
prudhvieswar00 <prudhvieswarmaganti00@gmail.com>
2025-12-26 16:03:47 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,14 @@
+class Student{
+    static String name;
+    Static String roll_no;
+    static int marks;
+
+    public static void main(String args[]){
+        Student prudhvi = new Student(prudhvi,23R21A05A4,84);
+        Student Nikhil = new Student(Nikhil,23R21A05A1,86);
+
+        System.out.println(prudhvi);
+        System.out.println(s2);
+        
+    }
+}
